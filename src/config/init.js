07afaf1c87f60fdf36c67aac44560e1b59975533
skycader <skycader@yandex.ru>
2024-06-10 11:06:14 +0300

    main: fix

--- src/config/init.js
+++ src/config/init.js
@@ -1,16 +1,22 @@
-const readline = require("readline-sync");
-const init = require("./replace.js");
+const readline = require('readline-sync');
+const init = require('./replace.js');
 
-console.log("🏗️ initializing project");
-const projectName = readline.question("📝 Project name: ");
-const appName = readline.question("📝 App name: ");
-const projectManufacturer = readline.question("📝 Manufacturer name: ");
+console.log('🏗️ initializing project');
+const projectName = readline.question('📝 Project name: ');
+const appName = readline.question('📝 App name: ');
+const projectManufacturer = readline.question('📝 Manufacturer name: ');
 
 let result = 1;
-result = result * init(/project-name/g, projectName, "package.json");
-result = result * init(/app-name/g, appName, "src/index.html");
-result = result * init(/app-name/g, appName, "src/config/app.config.ts");
-result = result * init(/project-manufacturer/g, projectManufacturer, "src/config/app.config.ts");
+result = result * init(/project-name/g, projectName, 'package.json');
+result = result * init(/app-name/g, appName, 'src/index.html');
+result = result * init(/app-name/g, appName, 'src/config/app.config.ts');
+result =
+  result *
+  init(
+    /project-manufacturer/g,
+    projectManufacturer,
+    'src/config/app.config.ts',
+  );
 
-if (result) console.log("✅ project has been successfuly initialized!");
-if !(result) console.log("❗Something went wrong")
+if (result) console.log('✅ project has been successfuly initialized!');
+if (!result) console.log('❗Something went wrong');
